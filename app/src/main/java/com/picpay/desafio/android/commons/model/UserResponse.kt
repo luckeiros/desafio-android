aa4f64b661484ee5bc1b43d07b768dd162df9e68
Lucas Ferreira <49406941+luckeiros@users.noreply.github.com>
2022-05-23 13:13:23 -0300

    removing useless serializable

--- app/src/main/java/com/picpay/desafio/android/commons/model/UserResponse.kt
+++ app/src/main/java/com/picpay/desafio/android/commons/model/UserResponse.kt
@@ -1,15 +1,8 @@
 package com.picpay.desafio.android.commons.model
 
-import com.google.gson.annotations.SerializedName
-import java.io.Serializable
-
 data class UserResponse(
-    @SerializedName("img")
     val img: String,
-    @SerializedName("name")
     val name: String,
-    @SerializedName("id")
     val id: Int,
-    @SerializedName("username")
     val username: String
-) : Serializable
+)
